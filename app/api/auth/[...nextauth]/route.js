746a4e1672750bbb370af7abbef92b3eb03d9eef
Agil Setiawan <agilsetiawan772@gmail.com>
2023-06-19 13:35:02 +0700

    Fix register user on google sign in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,37 +10,38 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-
-    session.user.id == sessionUser._id.toString();
-  },
-
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      // check if user exists
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
 
-      // if not, create new user and store to DB
-      if (!userExists) {
-        await User.create({
+      session.user.id == sessionUser._id.toString();
+    },
+
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // check if user exists
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
+
+        // if not, create new user and store to DB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
